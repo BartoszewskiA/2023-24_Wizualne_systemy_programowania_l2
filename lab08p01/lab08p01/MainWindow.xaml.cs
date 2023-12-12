58a779125754378179cc513e5bcb89f083b69e52
BartoszewskiA <bartoszewski.a@outlook.com>
2023-12-12 17:36:53 +0100

    12.12.2023

--- lab08p01/lab08p01/MainWindow.xaml.cs
+++ lab08p01/lab08p01/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,8 @@ namespace lab08p01
     /// </summary>
     public partial class MainWindow : Window
     {
+       
+        List<string> lista = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
@@ -30,8 +34,59 @@ namespace lab08p01
             OknoDialogowe oknoDialogowe = new OknoDialogowe();
             if(oknoDialogowe.ShowDialog()==true)
             {
-                tb_lista.Text += "\n" + oknoDialogowe.nazwa + " " + oknoDialogowe.sztuk.ToString();
+                //tb_lista.Text += "\n" + oknoDialogowe.nazwa + " " + oknoDialogowe.sztuk.ToString();
+                lista.Add(oknoDialogowe.nazwa + " " + oknoDialogowe.sztuk.ToString());
+                tb_lista.Inlines.Clear();
+          
+                foreach(string str in lista)
+                {
+                    tb_lista.Inlines.Add(str+"\n");                  
+                }
+
+                combo.Items.Clear();
+                foreach (string str in lista)
+                {
+                    combo.Items.Add(str);
+                }
+                combo.SelectedIndex = 0;
+               
             }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Lista zakuów (*.liz)|*.liz";
+            if(saveFileDialog.ShowDialog()==true)
+            {
+                StreamWriter sw = File.CreateText(saveFileDialog.FileName);
+                foreach (string str in lista)
+                {
+                    sw.WriteLine(str);
+                }
+                sw.Close();
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            int poz = combo.SelectedIndex;
+            if (poz < 0) 
+                return;
+            lista.RemoveAt(poz);
+            tb_lista.Inlines.Clear();
+
+            foreach (string str in lista)
+            {
+                tb_lista.Inlines.Add(str + "\n");
+            }
+
+            combo.Items.Clear();
+            foreach (string str in lista)
+            {
+                combo.Items.Add(str);
+            }
+            combo.SelectedIndex = 0;
         }
     }
 }

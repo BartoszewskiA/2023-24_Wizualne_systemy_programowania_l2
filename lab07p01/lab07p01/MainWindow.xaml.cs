83b797c031f2356e7c01b742a2d0e2f1f928ef2e
BartoszewskiA <bartoszewski.a@outlook.com>
2023-12-12 16:22:26 +0100

    12.122023

---
+++ lab07p01/lab07p01/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab07p01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        const String ET01 = "Liczba znaków: ";
+        const String ET02 = "Liczba znaków bez spacji: ";
+        const String ET03 = "Liczba akapitów: ";
+        public MainWindow()
+        {
+            InitializeComponent();
+            licznik_1.Text = ET01;
+            licznik_2.Text = ET02;
+            licznik_3.Text = ET03;
+        }
+
+        private void przelicz_Click(object sender, RoutedEventArgs e)
+        {
+            String s = poleTekstowe.Text;
+            int l_znakow = s.Length;
+            int l_spacji = 0;
+            int l_enterow = 0;
+
+            for (int i = 0; i <s.Length ; i++)
+                if (s[i]==' ') l_spacji++;
+            for (int i = 0; i < s.Length; i++)
+                if (s[i] == '\n') l_enterow++;
+
+            licznik_1.Text = ET01 + l_znakow;
+            licznik_2.Text=ET02 + (l_znakow - l_spacji).ToString();
+            licznik_3.Text = ET03 + l_enterow;
+        }
+    }
+}
---
+++ lab08p01/lab08p01/OknoDialogowe.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab08p01
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OknoDialogowe.xaml
+    /// </summary>
+    public partial class OknoDialogowe : Window
+    {
+        public String nazwa;
+        public int sztuk;
+        
+        public OknoDialogowe()
+        {
+            InitializeComponent();
+        }
+
+        private void button_ok_Click(object sender, RoutedEventArgs e)
+        {
+            if(textBox_nazwa.Text.Length == 0) 
+            {
+                MessageBox.Show("Pole Nazwa musi być wypełnione", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else
+            {
+                nazwa = textBox_nazwa.Text;
+            }
+            nazwa = textBox_nazwa.Text;
+            if (Int32.TryParse(textBox_sztuk.Text, out sztuk) != true)
+            {
+                MessageBox.Show("Pole Sztuk powinno zawierać liczbę", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            
+            this.DialogResult = true;
+        }
+    }
+}

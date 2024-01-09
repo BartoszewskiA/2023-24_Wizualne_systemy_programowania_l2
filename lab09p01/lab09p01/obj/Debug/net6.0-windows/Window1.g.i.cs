1fac63420604f88942e4ca620fb51f2648f8fa59
BartoszewskiA <bartoszewski.a@outlook.com>
2024-01-09 16:27:50 +0100

    09.01.2024

--- lab09p01/lab09p01/obj/Debug/net6.0-windows/Window1.g.i.cs
+++ lab09p01/lab09p01/obj/Debug/net6.0-windows/Window1.g.i.cs
@@ -61,7 +61,7 @@ namespace lab09p01 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/lab09p01;component/window1.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/lab09p01;V1.0.0.0;component/window1.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Window1.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
